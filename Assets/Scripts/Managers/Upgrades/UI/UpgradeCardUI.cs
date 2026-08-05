6cf47d7772669e6ca1b90c75504c829d03256584
Juanda999 <juanda999games@gmail.com>
2026-08-05 00:01:32 -0500

    Format upgrade values

--- Assets/Scripts/Managers/Upgrades/UI/UpgradeCardUI.cs
+++ Assets/Scripts/Managers/Upgrades/UI/UpgradeCardUI.cs
@@ -10,7 +10,19 @@ public class UpgradeCardUI : MonoBehaviour
     {
         attachedUpgradeData = upgrade;
         statNameText.text = upgrade.targetStat.displayName;
-        upgradeValueText.text = upgrade.upgradeValue.ToString("F2");
+        upgradeValueText.text = FormatUpgradeValue(upgrade.upgradeValue, upgrade.usedFlatScaling);
+    }
+
+    private string FormatUpgradeValue(float value, bool usedFlatScaling)
+    {
+        if (usedFlatScaling)
+        {
+            return $"+{value.ToString("0.##")}";
+        }
+        else
+        {
+            return $"{value.ToString("0.##")}%";
+        }
     }
 
     public void OnCardSelected()
