e7779aed3ec9f313b414cb0c20d50e5cb25b9c6a
Juanda999 <juanda999games@gmail.com>
2026-08-14 19:19:21 -0500

    Add logic to implement pause

--- Assets/Scripts/Managers/CursorManager.cs
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CursorManager : MonoBehaviour
+public static class CursorManager
 {
     public static void SetCursorVisible(bool visible)
     {
---
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class PauseManager : MonoBehaviour
+{
+    public static PauseManager Instance { get; private set; }
+    public event Action<bool> OnPauseChanged;
+    private List<IPausable> _pausables;
+    private bool _isPaused;
+
+    public bool IsPaused
+    {
+        get => _isPaused;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            _pausables = new List<IPausable>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            TogglePause();
+        }
+    }
+
+    public void Register(IPausable pausable)
+    {
+        if (pausable != null && !_pausables.Contains(pausable))
+        {
+            _pausables.Add(pausable);
+        }
+    }
+
+    public void Unregister(IPausable pausable)
+    {
+        if (pausable != null && _pausables.Contains(pausable))
+        {
+            _pausables.Remove(pausable);
+        }
+    }
+
+    public void TogglePause()
+    {
+        SetPause(!_isPaused);
+    }
+
+    public void SetPause(bool pauseState)
+    {
+        _isPaused = pauseState;
+        Time.timeScale = _isPaused ? 0f : 1f;
+
+        CursorManager.SetCursorVisible(_isPaused);
+
+        for (int i = 0; i < _pausables.Count; i++)
+        {
+            if (_pausables[i] != null)
+            {
+                if (_isPaused)
+                {
+                    _pausables[i].OnPause();
+                }
+                else
+                {
+                    _pausables[i].OnResume();
+                }
+            }
+        }
+
+        OnPauseChanged?.Invoke(_isPaused);
+    }
+}
