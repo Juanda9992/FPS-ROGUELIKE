2b4ad5f91243a043f7d5067d0d978333da9e1f4b
Juanda999 <juanda999games@gmail.com>
2026-08-18 23:10:01 -0500

    FIx bug with player making double jump

--- Assets/Scripts/Managers/CursorManager.cs
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -4,7 +4,6 @@ public static class CursorManager
 {
     public static void SetCursorVisible(bool visible)
     {
-        Debug.Log("Cursor is visible: " + visible);
         Cursor.visible = visible;
         Cursor.lockState = visible ? CursorLockMode.None : CursorLockMode.Locked;
     }
